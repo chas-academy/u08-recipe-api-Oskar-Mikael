47330cdf1919b1c50dd663eebfb9947f6912d658
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-04-23 16:28:48 +0200

    Recipes relation

--- app/Models/RecipeList.php
+++ app/Models/RecipeList.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Recipe;
 
 class RecipeList extends Model
 {
@@ -17,5 +18,10 @@ class RecipeList extends Model
     public function user() 
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function recipes()
+    {
+        return $this->hasMany(Recipe::class);
     }
 }
